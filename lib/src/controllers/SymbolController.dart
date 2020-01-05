76fbea4c518096a6250ac2413a102b65a5c61712
Sergio Orellana <orellana123@gmail.com>
2020-01-05 16:20:02 -0300

    Se agrega manejo de iconos de bomberos.

--- lib/src/controllers/SymbolController.dart
+++ lib/src/controllers/SymbolController.dart
@@ -1,13 +1,23 @@
+import 'package:mapbox_gl/mapbox_gl.dart' as mapBox;
+
 class SymbolController {
-
-  List<Symbol> bomberosSymbols;
-  List<Symbol> carabinerosSymbols;
-  List<Symbol> urgenciasSymbols;
-
+  List<mapBox.Symbol> urgenciasSymbols;
+  List<mapBox.Symbol> bomberosSymbols;
+  List<mapBox.Symbol> carabinerosSymbols;
+  
+  bool showUrgencias;
   bool showBomberos;
   bool showCarabineros;
-  bool showUrgencias;
+  
 
-  SymbolController();
+  SymbolController()
+  {
+    urgenciasSymbols = new List<mapBox.Symbol>();
+    bomberosSymbols = new List<mapBox.Symbol>();
+    carabinerosSymbols = new List<mapBox.Symbol>();
+    showUrgencias = false;
+    showBomberos = false;
+    showCarabineros = false;
+  }
   
 }
--- lib/src/pages/HomePage.dart
+++ lib/src/pages/HomePage.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
+import 'dart:ffi';
 
+import 'package:chilemergencias/src/controllers/SymbolController.dart';
 import 'package:chilemergencias/src/models/BomberosModel.dart';
 import 'package:chilemergencias/src/models/CarabinerosModel.dart';
 import 'package:chilemergencias/src/models/UrgenciasModel.dart';
@@ -16,41 +18,44 @@ class HomePage extends StatefulWidget {
 //https://stackoverflow.com/questions/54910211/errorflutter-lib-ui-ui-dart-state-cc148-unhandled-exception/57087364
 
 class _HomePageState extends State<HomePage> {
+  bool _areMarkersDrawed = false;
+  SymbolController _symbolController;
   LatLng _myPosition;
   MapboxMapController _mapController;
   MapboxMap _mapBoxMap;
   Map<String, dynamic> _allData;
-  List<Symbol> _markers = new List();
+  
+  Color _showFilterUrgenciasButtonColor   = Colors.white54;
+  Color _showFilterBomberosButtonColor    = Colors.white54;
+  Color _showFilterCarabinerosButtonColor = Colors.white54;
+
 
   @override
   void initState() {
     super.initState();
     _myPosition = new LatLng(0, 0);
+    _createMapBoxMap();
+    _symbolController = new SymbolController();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(title: Text("Hi")),
-      body: Stack(children: <Widget>[
-        mapWithStreaming(),
-        drawInstitutionsOnMap()
-      ],
-        
-        
-      ),
-      floatingActionButton: _centerGpsCamera()      
-    );
+        // appBar: AppBar(title: Text("Hi")),
+        body: Stack(
+          children: <Widget>[mapWithStreaming(),
+                            _filterButtons()],
+        ),
+        floatingActionButton: _centerGpsCamera());
   }
 
   FloatingActionButton _centerGpsCamera() {
     return FloatingActionButton(
-      onPressed: () async {
-      await _mapController.moveCamera(CameraUpdate.newLatLng(_myPosition));
-    },
-    child: Icon(Icons.gps_fixed),
-    
-    
+      onPressed: checkInformation,
+      //   onPressed: () async {
+      //   await _mapController.moveCamera(CameraUpdate.newLatLng(_myPosition));
+      // },
+      child: Icon(Icons.gps_fixed),
     );
   }
 
@@ -61,11 +66,17 @@ class _HomePageState extends State<HomePage> {
       stream: location.onLocationChanged(),
       builder: (BuildContext context, AsyncSnapshot<LocationData> snapshot) {
         if (snapshot.hasData) {
+          Widget widget = Container();
           _myPosition = LatLng(snapshot.data.latitude, snapshot.data.longitude);
-          if (_mapBoxMap == null) {
-            _createMapBoxMap();
+
+          if (_mapController != null && _areMarkersDrawed == false) {
+            widget = drawInstitutionsOnMap();
+            _areMarkersDrawed = checkMarkersInitialized();
           }
-          return _mapBoxMap;
+
+          return Stack(
+            children: <Widget>[_mapBoxMap, widget],
+          );
         } else {
           return Center(child: CircularProgressIndicator());
         }
@@ -75,78 +86,168 @@ class _HomePageState extends State<HomePage> {
 
   void _createMapBoxMap() {
     _mapBoxMap = MapboxMap(
-      initialCameraPosition: CameraPosition(target: _myPosition, zoom: 10),
-      myLocationEnabled: true,
-      onMapCreated: (controller) {
-        _mapController = controller;
-      },
-      compassEnabled: true,
-      //minMaxZoomPreference: MinMaxZoomPreference(),
-      //onCameraTrackingDismissed: (){print("Salimos del foco perrito");},
-      rotateGesturesEnabled: false,
-      tiltGesturesEnabled: true,
-      zoomGesturesEnabled: true
-    );
+        initialCameraPosition: CameraPosition(target: _myPosition, zoom: 10),
+        myLocationEnabled: true,
+        onMapCreated: (controller) {
+          _mapController = controller;
+        },
+        compassEnabled: true,
+        //minMaxZoomPreference: MinMaxZoomPreference(),
+        //onCameraTrackingDismissed: (){print("Salimos del foco perrito");},
+        rotateGesturesEnabled: false,
+        tiltGesturesEnabled: true,
+        zoomGesturesEnabled: true);
   }
 
   //TODO: Controlar que map controller no sea null mientras se tenga data.
-  Widget drawInstitutionsOnMap()
-  {
+  Widget drawInstitutionsOnMap() {
     return FutureBuilder(
       future: provider.allDataToMap(),
-      builder: (BuildContext context, AsyncSnapshot<Map<String,dynamic>> snapshot) {
-        if (snapshot.hasData && _mapController != null)
-        {
-          for (BomberosModel item in provider.bomberos) {
-            _addSymbol("fire-station-15", LatLng(item.latitude, item.longitude));
-          }
-
-          for (CarabinerosModel item in provider.carabineros) {
-            _addSymbol("police-15", LatLng(item.latitude, item.longitude));
-          }
-
+      builder:
+          (BuildContext context, AsyncSnapshot<Map<String, dynamic>> snapshot) {
+        if (snapshot.hasData) {
+          //TODO: verificar que future no esté dando problemas por asignación a variable simple.
+          _addMarkers("Urgencias");
+          _symbolController.showUrgencias = true;
+          _addMarkers("Bomberos");
+          _symbolController.showBomberos = true;
+          _addMarkers("Carabineros");
+          _symbolController.showCarabineros = true;
 
           return Container();
-
-          // snapshot.data.forEach((k,v)
-          // {
-          //   _mapController.addSymbol(SymbolOptions(
-          //     provider.
-          //   ));
-
-          // }
-          // );
-
-        }
-        else
-        {
+        } else {
           return Center(child: CircularProgressIndicator());
         }
       },
     );
-
   }
 
-  void _addCircle(CarabinerosModel item)
-  {
-
+  void _addCircle(CarabinerosModel item) {
     _mapController.addCircle(
       CircleOptions(
-        geometry: LatLng(item.latitude,item.longitude),
+        geometry: LatLng(item.latitude, item.longitude),
         circleColor: "#FF0000",
       ),
     );
   }
 
-  void _addSymbol(String iconImage, LatLng latLng) {
-    _mapController.addSymbol(
-      SymbolOptions(
-        geometry: latLng,
-        iconImage: iconImage,
-        iconColor: "#FF0000",
-        iconHaloColor: "#FF0000"
+  Future<Symbol> _addSymbol(String iconImage, LatLng latLng) async {
+    return _mapController.addSymbol(
+      SymbolOptions(geometry: latLng, iconImage: iconImage),
+    );
+  }
+
+  Future<void> _removeSymbol(Symbol symbol) async {
+    return _mapController.removeSymbol(symbol);
+  }
+
+  void _addMarkers(String institution) async {
+    switch (institution) {
+      case "Urgencias":
+          for (UrgenciasModel item in provider.urgencias)
+            _symbolController.urgenciasSymbols.add(await _addSymbol(
+                "hospital-15", LatLng(item.latitude, item.longitude)));
+        break;
+      case "Bomberos":
+        if(_symbolController.bomberosSymbols.length == 0)
+        {
+          for (BomberosModel item in provider.bomberos)
+          {
+            _symbolController.bomberosSymbols.add(await _addSymbol(
+                "fire-station-15", LatLng(item.latitude, item.longitude)));
+          }
+        }
+        break;
+      case "Carabineros":
+          for (CarabinerosModel item in provider.carabineros)
+            _symbolController.carabinerosSymbols.add(await _addSymbol(
+                "police-15", LatLng(item.latitude, item.longitude)));
+        break;
+      default:
+        throw new Exception("Opción inválida");
+        break;
+    }
+  }
+
+  //TODO: Agregar verificación
+  void _deleteMarkers(String institution) async {
+    switch (institution) {
+      case "Urgencias":
+          for (Symbol item in _symbolController.urgenciasSymbols)
+            await _removeSymbol(item);
+        break;
+      case "Bomberos":
+          List<String> symbolIds = new List<String>();
+          for (Symbol item in _symbolController.bomberosSymbols)
+          {
+            symbolIds.add(item.id);
+          }
+          await _mapController.customClearSymbols(symbolIds);
+          _symbolController.bomberosSymbols = new List<Symbol>();
+            
+        break;
+      case "Carabineros":
+          for (Symbol item in _symbolController.carabinerosSymbols)
+            await _removeSymbol(item);
+        break;
+      default:
+        throw new Exception("Opción inválida");
+        break;
+    }
+  }
+
+  bool checkMarkersInitialized() {
+    if (_symbolController.showBomberos == true &&
+        _symbolController.showUrgencias &&
+        _symbolController.showCarabineros) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Widget _filterButtons()
+  {
+    FloatingActionButton filterUrgencias = new FloatingActionButton(
+      child: Image(image: AssetImage("assets/urgencias_Chile.png"), height: 45.0, width: 45.0,),
+      onPressed: (){
         
-      ),
+      },
+      backgroundColor: _showFilterUrgenciasButtonColor,
+    );
+
+    FloatingActionButton filterBomberos = new FloatingActionButton(
+      child: Image(image: AssetImage("assets/Bomberos_Chile.png"),),
+      onPressed: (){
+        setState(() {
+          _symbolController.showBomberos = !_symbolController.showBomberos; 
+          (_symbolController.showBomberos) ? _showFilterBomberosButtonColor = Colors.white54 : _showFilterBomberosButtonColor = Colors.grey;
+          (_symbolController.showBomberos) ? _addMarkers("Bomberos") : _deleteMarkers("Bomberos");
+        });
+      },
+      backgroundColor: _showFilterBomberosButtonColor
+    );
+
+    FloatingActionButton filterCarabineros = new FloatingActionButton(
+      child: Image(image: AssetImage("assets/carabineros_Chile.png"),),
+      onPressed: (){},
+      backgroundColor: _showFilterCarabinerosButtonColor
+    );
+
+    return Column(
+      children: <Widget>[
+        filterUrgencias,
+        SizedBox(
+          height: 10.0,
+        ),
+        filterBomberos,
+        SizedBox(
+          height: 10.0,
+        ),
+        filterCarabineros
+      ],
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
     );
   }
 
@@ -154,5 +255,15 @@ class _HomePageState extends State<HomePage> {
   void dispose() {
     _mapController.dispose();
     super.dispose();
+  }
+
+  checkInformation() {
+    _areMarkersDrawed;
+    _symbolController;
+    _myPosition;
+    _mapController;
+    _mapBoxMap;
+    _allData;
+    return Container();
   }
 }
